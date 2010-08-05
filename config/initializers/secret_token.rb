314ff0c391876d32093dcaf5274c3e7a71df27e6
Iver Dihle Skjervum <iver.ds@gmail.com>
2010-08-05 14:21:19 +0200

    First setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WwwIsfitOrg::Application.config.secret_token = '7a9017fd8db6e4ec67666ddbe48aeb9042b93a5b57c92b5c5b1014c46023b8b049a1b3277ec50044f764bd8730254071c9d38940339e2fff71f17ca7e69aac5d'
