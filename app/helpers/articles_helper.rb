770ade42efee0e3567e5154431e073087b64049e
iverds <iver.ds@gmail.com>
2010-08-16 19:56:41 +0200

    Nothing

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -5,8 +5,10 @@ module ArticlesHelper
 
   def format(str)
     set_correct_tags(str)
-    bc = BlueCloth.new(str)
+    reg = Regexp.new(/\[(\w+)\]\(([A-Za-z0-9_:.\/]+)\)/)
+    str.gsub!(reg, "<a href=\"\\2\" target=\"_blank\">\\1</a>")
 
+    bc = BlueCloth.new(str) 
     #str.gsub!(/([^>])\s*\r\n\r\n\s*([^<])/m, "\\1<br /><br />\\2")
     #str.gsub!(/([^>])\s*\r\n\s*([^<])/m, "\\1<br />\\2")
     #str
