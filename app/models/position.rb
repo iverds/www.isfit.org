2c76585442941d990ec326713f9b4bc324b18b7b
iverds <iver.ds@gmail.com>
2010-08-31 07:15:00 +0200

    No admission

--- app/models/position.rb
+++ app/models/position.rb
@@ -5,10 +5,10 @@ class Position < ActiveRecord::Base
         lang_attr :title, :description
 
   def self.find_all_active_positions
-    positions = Position.find_all_by_admission_id(1)
+    positions = Position.find_all_by_admission_id(2)
     positions.sort_by {|x| [(x.group.section == nil ? "" :  x.group.section.name_no), x.group.name_no] }
   end
   def self.find_all_active_positions_alfa #sorterer alfabetisk
-    positions = Position.find_all_by_admission_id(1, :order => "title_no")
+    positions = Position.find_all_by_admission_id(2, :order => "title_no")
   end
 end
