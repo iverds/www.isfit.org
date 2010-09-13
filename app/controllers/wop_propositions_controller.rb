e6b309cde4269cc72f8ec376f3d641f463ab9151
iverds <iver.ds@gmail.com>
2010-09-13 22:31:14 +0200

    Disabled wop-compotition

--- app/controllers/wop_propositions_controller.rb
+++ app/controllers/wop_propositions_controller.rb
@@ -6,17 +6,7 @@ class WopPropositionsController < ApplicationController
 
   def create
     @wop_proposition = WopProposition.new(params[:wop_proposition])
-
-   
-
     if @wop_proposition.valid? && verify_recaptcha(:model=>@wop_proposition, :message=>"Recaptcha gjenkjenning feilet") && @wop_proposition.save
-      #unless params[:wop_proposition][:attachment] ==""
-      #path = Rails.root+"/public/images/article"+@wop_proposition.id
-      #f = DataFile.new(path,params[:wop_proposition][:attachment]) 
-      #f.save
-
-      #end
-
       flash[:notice] = "Forslaget ble sendt"
       redirect_to :tab=>params[:tab], :action=>"new"
     else
