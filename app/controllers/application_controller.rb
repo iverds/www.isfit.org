c791a5239dd81ea74547b5ef368527d3fe466a13
iverds <iver.ds@gmail.com>
2010-09-24 14:35:33 +0200

    Added left to apply

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,20 @@ class ApplicationController < ActionController::Base
   helper :all
 
   protect_from_forgery
-  before_filter  :set_language
+  before_filter  :set_language, :set_days_until
+
+  def set_days_until
+  difference = (Time.local(2010,"sep",30,23,59,59)-Time.now)
+
+  @seconds    =  difference % 60
+  difference = (difference - @seconds) / 60
+  @minutes    =  difference % 60
+  difference = (difference - @minutes) / 60
+  @hours      =  difference % 24
+  difference = (difference - @hours)   / 24
+  @days       =  difference % 7
+  end
+
   def set_language
     unless session[:locale]
       country_code = get_country_code_by_id(request.remote_ip)
